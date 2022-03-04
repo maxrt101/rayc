1acb7c3dab4b4ed6f9d603b08262ad8269148fd6
Maksym Tkachuk <s2m4f6@gmail.com>
2022-03-04 21:32:37 +0200

    Removed old project files (backup in origin/old)

--- rayc/core/event.cc
+++
@@ -1,9 +0,0 @@
-#include "core/event.h"
-
-rayc::Event::Event(const std::string& name) : m_name(name) {}
-
-rayc::Event::~Event() {}
-
-std::string rayc::Event::getName() const {
-  return m_name;
-}
--- rayc/core/input.cc
+++
@@ -1,6 +0,0 @@
-#include "core/input.h"
-
-rayc::KeyState::KeyState() {}
-
-rayc::KeyState::KeyState(bool p, bool h, bool r)
-  : pressed(p), held(h), released(r) {}
